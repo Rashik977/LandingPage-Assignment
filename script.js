8984a93ace3f4e23d35158766bf1af046cd4e247
rasik <rkoirala43@gmail.com>
2024-06-03 17:32:05 +0545

    media queries

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+const toggle = document.querySelector(".nav__toggle");
+const toggle_icon = document.querySelector(".nav__toggle-icon");
+
+toggle.addEventListener("click", () => {
+  console.log("clicked");
+  if (body.classList.length) {
+    toggle_icon.src = "./assets/light.png";
+    body.classList.remove("dark-mode");
+  } else {
+    toggle_icon.src = "./assets/dark.png";
+    body.classList.add("dark-mode");
+  }
+});
